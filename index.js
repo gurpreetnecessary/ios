d77a331d58ceb6c263d23222030ae039d387ac83
gurpreetnecessary <55244073+gurpreetnecessary@users.noreply.github.com>
2019-11-29 13:59:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Detects if device is on iOS 
 const isIos = () => {
   const userAgent = window.navigator.userAgent.toLowerCase();
+  alert(userAgent);
   return /iphone|ipad|ipod/.test( userAgent );
 }
 // Detects if device is in standalone mode
@@ -10,3 +11,15 @@ const isInStandaloneMode = () => ('standalone' in window.navigator) && (window.n
 if (isIos() && !isInStandaloneMode()) {
   this.setState({ showInstallMessage: true });
 }
+
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('service-worker.js');
+}
+
+document.querySelector('#show').addEventListener('click', () => {
+  const iconUrl = document.querySelector('select').selectedOptions[0].value;
+  let imgElement = document.createElement('img');
+  imgElement.src = iconUrl;
+  document.querySelector('#container').appendChild(imgElement);
+});
